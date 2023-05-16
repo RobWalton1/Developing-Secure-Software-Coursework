80256c868c2a2f91a867cfcebdaecdbd9e497fbd
Rob Walton <robwalton06@gmail.com>
2023-05-16 12:38:27 +0100

    Account enumeration test added

--- test/accountEnumeration.test.js
+++ test/accountEnumeration.test.js
@@ -1,1 +1,18 @@
 const assert = require('assert');
+const request = require('supertest');
+const app = require('../index.js')
+
+describe('Account Enumeration test', () => {
+    it('should return generic message', function(){
+        const username = 'test';
+        const password = 'pass';
+        const authCode = '1234';
+        request(app)
+        .post('/login')
+        .send({username, password, authenticationCode: authCode})
+        .end((err, res) => {
+        assert.equal(res.body.message, 'The username, password and/or authentication code are incorrect. Please try again. ');
+        done(err)
+    });
+});
+});
